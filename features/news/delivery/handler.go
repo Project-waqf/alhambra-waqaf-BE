1d481c31f3a56685e74bca37468e0cfd5d6800cc
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-22 11:19:35 +0700

    upload  image to imagekit.io

--- features/news/delivery/handler.go
+++ features/news/delivery/handler.go
@@ -23,12 +23,23 @@ func New(e *echo.Echo, data domain.UseCaseInterface) {
 func (news *NewsDelivery) AddNews() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var input News
+
+		file, fileheader, err := c.Request().FormFile("picture")
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, helper.Failed("Error input"))
+		}
 		
-		err := c.Bind(&input)
+		filename, err := helper.Upload(c, file, fileheader)
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, helper.Failed("Error input"))
 		}
 
+		err = c.Bind(&input)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, helper.Failed("Error input"))
+		}
+		
+		input.Picture = filename
 		cnv := ToDomain(input)
 		res, err := news.NewsServices.AddNews(cnv)
 		if err != nil {
--- helper/upload.go
+++ helper/upload.go
@@ -3,17 +3,38 @@ package helper
 import (
 	"bytes"
 	"context"
+	"math/rand"
 	"io"
 	"mime/multipart"
 	"os"
+	"time"
 
 	"github.com/codedius/imagekit-go"
 	"github.com/labstack/echo/v4"
 )
 
+// CREATE RANDOM STRING
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func autoGenerate(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func String(length int) string {
+	return autoGenerate(length, charset)
+}
 
 
-func Upload(c echo.Context, file multipart.File, fileheader *multipart.FileHeader) error{
+func Upload(c echo.Context, file multipart.File, fileheader *multipart.FileHeader) (string, error){
 	opts := imagekit.Options{
 		PublicKey:  os.Getenv("IMAGEIO_PUBLIC"),
 		PrivateKey: os.Getenv("IMAGEIO_PRIVATE"),
@@ -24,15 +45,17 @@ func Upload(c echo.Context, file multipart.File, fileheader *multipart.FileHeade
 		panic(err)
 	}
 
+	fileName := autoGenerate(10, fileheader.Filename)
+
 	buf := bytes.NewBuffer(nil)
 	if _, err := io.Copy(buf, file); err != nil {
-		return err
+		return "", err
 	}
 	
 
 	ur := imagekit.UploadRequest{
 		File:              buf.Bytes(), // []byte OR *url.URL OR url.URL OR base64 string
-		FileName:          fileheader.Filename,
+		FileName:          fileName,
 		UseUniqueFileName: false,
 		Tags:              []string{},
 		Folder:            "/",
@@ -43,9 +66,9 @@ func Upload(c echo.Context, file multipart.File, fileheader *multipart.FileHeade
 	
 	ctx := context.Background()
 	
-	_, err = ik.Upload.ServerUpload(ctx, &ur)
+	r, err := ik.Upload.ServerUpload(ctx, &ur)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	return r.URL, nil
 }
