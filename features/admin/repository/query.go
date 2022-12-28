af2000ddba19518c43629f8216254d4c47001777
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-28 10:23:42 +0700

    login admin with password

--- features/admin/repository/query.go
+++ features/admin/repository/query.go
@@ -19,7 +19,7 @@ func New(db *gorm.DB) domain.RepoInterface {
 func (repo *AdminRepository) Login(data domain.Admin) (domain.Admin, error) {
 	input := FromDomainLogin(data)
 	
-	if err := repo.db.Where("username", input.Username).First(&input).Error; err != nil {
+	if err := repo.db.Where("username = ? AND password = ?", input.Username, input.Password).First(&input).Error; err != nil {
 		return domain.Admin{}, err
 	}
 
