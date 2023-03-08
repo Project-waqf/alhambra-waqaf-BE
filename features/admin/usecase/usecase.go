cf7d6848b5f675a13c3c39b8e88207e0edd62ae0
faturfawkes <nur.fatchurohman@salt.co.id>
2023-03-08 23:48:48 +0700

    testing redis 2

--- features/admin/usecase/usecase.go
+++ features/admin/usecase/usecase.go
@@ -95,7 +95,7 @@ func (u *AdminServices) ForgotSendEmail(input domain.Admin) (domain.Admin, error
 
 	// Save To Redis
 	redis := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "localhost:6389",
 		Password: "",
 		DB:       0,
 	})
