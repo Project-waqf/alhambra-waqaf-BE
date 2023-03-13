284ac7c524ce34aebdcf18484373dff76b862b01
faturfawkes <nur.fatchurohman@salt.co.id>
2023-03-13 21:23:38 +0700

    remove print secret key

--- features/admin/usecase/usecase.go
+++ features/admin/usecase/usecase.go
@@ -95,7 +95,6 @@ func (u *AdminServices) ForgotSendEmail(input domain.Admin) (domain.Admin, error
 	}
 
 	// Save To Redis
-	fmt.Println("INI SECRET KEYS", os.Getenv("SECRET_KEY"))
 	token := encrypt([]byte(os.Getenv("SECRET_KEY")), input.Email)
 	err = u.AdminRepository.SaveRedis(input.Email, token)
 	if err != nil {
