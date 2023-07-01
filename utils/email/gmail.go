037d2181cd54d4dbde1d7b53ea1c36b349411129
faturfawkes <nur.fatchurohman@salt.co.id>
2023-07-02 01:11:34 +0700

    test smpt gmail

--- utils/email/gmail.go
+++ utils/email/gmail.go
@@ -1,15 +1,12 @@
 package email
 
 import (
-	"crypto/tls"
-	"encoding/base64"
 	"fmt"
 	"net/smtp"
 	"os"
 	"strconv"
 
 	"go.uber.org/zap"
-	"golang.org/x/oauth2"
 )
 
 // var emailAuth smtp.Auth
@@ -42,6 +39,7 @@ const (
 )
 
 func SendOtpGmail(email, token string, logger *zap.Logger) error {
+	logger.Info("Sending Email...")
 	body := GetBody(token)
 
 	// Create the email message.
@@ -50,55 +48,13 @@ func SendOtpGmail(email, token string, logger *zap.Logger) error {
 		fmt.Sprintf("Subject: %s\r\n\r\n", subject) +
 		body
 
-	// Obtain an access token.
-	tokenGoogle := &oauth2.Token{AccessToken: os.Getenv("PASSWORD_GOOGLE")}
+	// Authentication.
+	auth := smtp.PlainAuth("", senderEmail, os.Getenv("PASSWORD_GOOGLE"), smtpHost)
 
-	// Construct the XOAUTH2 authentication header.
-	authString := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s\000%s\000%s", senderEmail, tokenGoogle.AccessToken, tokenGoogle.AccessToken)))
-
-	// Connect to the SMTP server.
-	client, err := smtp.Dial(smtpHost + ":" + strconv.Itoa(smtpPort))
-	if err != nil {
-		logger.Error("Error SMTP Dial", zap.Error(err))
+	addr := smtpHost + ":" + strconv.Itoa(smtpPort)
+	if err := smtp.SendMail(addr, auth, senderEmail, []string{email}, []byte(message)); err != nil {
+		logger.Fatal("Error Send Email", zap.Error(err))
 	}
-	defer client.Close()
-
-	// Start TLS encryption.
-	tlsConfig := &tls.Config{
-		InsecureSkipVerify: true,
-	}
-
-	if err = client.StartTLS(tlsConfig); err != nil {
-		logger.Fatal("Error TLS", zap.Error(err))
-	}
-
-	// Authenticate with the server using the XOAUTH2 header.
-	if err = client.Auth(smtp.PlainAuth("", senderEmail, authString, smtpHost)); err != nil {
-		logger.Error("Error Auth", zap.Error(err))
-	}
-
-	// Set the sender and recipient.
-	if err = client.Mail(senderEmail); err != nil {
-		logger.Error("Error Mail", zap.Error(err))
-	}
-	if err = client.Rcpt(recipientEmail); err != nil {
-		logger.Error("Error RCPT", zap.Error(err))
-	}
-
-	// Send the email.
-	w, err := client.Data()
-	if err != nil {
-		logger.Error("Error Send Email", zap.Error(err))
-	}
-	_, err = w.Write([]byte(message))
-	if err != nil {
-		logger.Error("Error Write Message", zap.Error(err))
-	}
-	err = w.Close()
-	if err != nil {
-		logger.Error("Error Close Write", zap.Error(err))
-	}
-
 	logger.Info("Email sent successfully!")
 	return nil
 }
