3b69c9e1dfe5059993f348ea51da59251c5e3242
faturfawkes <nur.fatchurohman@salt.co.id>
2023-07-02 01:49:07 +0700

    send email with gmail api

--- utils/email/gmail.go
+++ utils/email/gmail.go
@@ -1,12 +1,17 @@
 package email
 
 import (
+	"context"
+	"encoding/base64"
 	"fmt"
-	"net/smtp"
+	"log"
 	"os"
-	"strconv"
 
 	"go.uber.org/zap"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
 )
 
 // var emailAuth smtp.Auth
@@ -38,23 +43,100 @@ const (
 	subject        = "Reset Password Admin wakafalhambra.com"
 )
 
+// func SendOtpGmail(email, token string, logger *zap.Logger) error {
+// 	logger.Info("Sending Email...")
+// 	body := GetBody(token)
+
+// 	// Create the email message.
+// 	message := fmt.Sprintf("From: %s\r\n", senderEmail) +
+// 		fmt.Sprintf("To: %s\r\n", recipientEmail) +
+// 		fmt.Sprintf("Subject: %s\r\n\r\n", subject) +
+// 		body
+
+// 	// Authentication.
+// 	auth := smtp.PlainAuth("", senderEmail, os.Getenv("PASSWORD_GOOGLE"), smtpHost)
+
+// 	addr := smtpHost + ":" + strconv.Itoa(smtpPort)
+// 	if err := smtp.SendMail(addr, auth, senderEmail, []string{email}, []byte(message)); err != nil {
+// 		logger.Fatal("Error Send Email", zap.Error(err))
+// 	}
+// 	logger.Info("Email sent successfully!")
+// 	return nil
+// }
+
 func SendOtpGmail(email, token string, logger *zap.Logger) error {
 	logger.Info("Sending Email...")
+	ctx := context.Background()
+
+	// Read your credentials file (JSON format)
+	credentialsFile := "/gmail.json"
+	credentials, err := os.ReadFile(credentialsFile)
+	if err != nil {
+		log.Fatalf("Failed to read credentials file: %v", err)
+		return err
+	}
+	// Retrieve the OAuth 2.0 token
+	config, err := google.ConfigFromJSON(credentials, gmail.MailGoogleComScope)
+	if err != nil {
+		log.Fatalf("Failed to read credentials file: %v", err)
+		return err
+	}
+
+	// Exchange the authorization code for a token
+	tokenGoogle := getTokenFromAuthorizationCode(ctx, config)
+
+	// Create the HTTP client with the token
+	client := config.Client(ctx, tokenGoogle)
+
+
+	// Create the Gmail API client with the HTTP client
+	gmailService, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Failed to create Gmail service: %v", err)
+	}
+	
+	// Compose the email
 	body := GetBody(token)
 
 	// Create the email message.
-	message := fmt.Sprintf("From: %s\r\n", senderEmail) +
+	messagePlain := fmt.Sprintf("From: %s\r\n", senderEmail) +
 		fmt.Sprintf("To: %s\r\n", recipientEmail) +
 		fmt.Sprintf("Subject: %s\r\n\r\n", subject) +
 		body
+	message := createMessage(senderEmail, email, subject, messagePlain)
 
-	// Authentication.
-	auth := smtp.PlainAuth("", senderEmail, os.Getenv("PASSWORD_GOOGLE"), smtpHost)
-
-	addr := smtpHost + ":" + strconv.Itoa(smtpPort)
-	if err := smtp.SendMail(addr, auth, senderEmail, []string{email}, []byte(message)); err != nil {
-		logger.Fatal("Error Send Email", zap.Error(err))
+	// Send the email
+	_, err = gmailService.Users.Messages.Send("me", message).Do()
+	if err != nil {
+		log.Fatalf("Failed to send email: %v", err)
 	}
-	logger.Info("Email sent successfully!")
+
+	fmt.Println("Email sent successfully!")
 	return nil
+}
+
+func getTokenFromAuthorizationCode(ctx context.Context, config *oauth2.Config) *oauth2.Token {
+	// Read the authorization code (obtained from the OAuth 2.0 flow)
+	var authCode string
+	fmt.Print("Enter the authorization code: ")
+	fmt.Scanln(&authCode)
+
+	// Exchange the authorization code for a token
+	token, err := config.Exchange(ctx, authCode)
+	if err != nil {
+		log.Fatalf("Failed to exchange authorization code: %v", err)
+	}
+
+	return token
+}
+
+func createMessage(sender, recipient, subject, body string) *gmail.Message {
+	msg := &gmail.Message{
+		Raw: base64.StdEncoding.EncodeToString([]byte(
+			fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s",
+				sender, recipient, subject, body,
+			))),
+	}
+
+	return msg
 }
